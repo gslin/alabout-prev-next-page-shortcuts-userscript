57b201e5944382a38a8be4f68265039e28977dfe
Gea-Suan Lin <gslin@gslin.org>
2020-11-27 06:59:18 +0800

    Import.

---
+++ alabout-prev-next-page-shortcuts-userscript.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Alabout Prev/Next Page
+// @namespace   https://github.com/gslin/alabout-prev-next-page-shortcuts-userscript
+// @match       http://alabout.com/list.php*
+// @grant       none
+// @version     0.20201127.0
+// @author      Gea-Suan Lin <gslin@gslin.org>
+// @description Alabout prev/next page shortcuts.
+// @license     MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('keydown', ev => {
+        if ('<' === ev.key) {
+            let el = jQuery('a[href^="list.php"]:contains(前ページ)')[0];
+            if (el) {
+                el.click();
+            }
+            return;
+        }
+
+        if ('>' === ev.key) {
+            let el = jQuery('a[href^="list.php"]:contains(次ページ)')[0];
+            if (el) {
+                el.click();
+            }
+            return;
+        }
+    });
+})();
