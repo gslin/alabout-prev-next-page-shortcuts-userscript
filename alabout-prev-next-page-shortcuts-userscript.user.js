8964f0a75a519682eb0aeae8ff3a88d7dd64286f
Gea-Suan Lin <gslin@gslin.org>
2020-12-13 00:49:10 +0800

    Only effect with non-input & non-textarea focus.

--- alabout-prev-next-page-shortcuts-userscript.user.js
+++ alabout-prev-next-page-shortcuts-userscript.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/alabout-prev-next-page-shortcuts-userscript
 // @match       http://alabout.com/list.php*
 // @grant       none
-// @version     0.20201127.0
+// @version     0.20201213.0
 // @author      Gea-Suan Lin <gslin@gslin.org>
 // @description Alabout prev/next page shortcuts.
 // @license     MIT
@@ -13,6 +13,11 @@
     'use strict';
 
     window.addEventListener('keydown', ev => {
+        let aEl = document.activeElement;
+        if ('input' === aEl.tagName.toLowerCase() || 'textarea' === aEl.tagName.toLowerCase()) {
+            return;
+        }
+
         if ('<' === ev.key) {
             let el = jQuery('a[href^="list.php"]:contains(前ページ)')[0];
             if (el) {
